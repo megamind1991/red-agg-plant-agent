1b629ba315a74fdc7a965e2395e9b01b86272b50
0006841 <youmianjin@suiyi.com.cn>
2022-07-05 15:48:15 +0800

    v4

---
+++ redaggr-agent/src/test/java/com/shlf/promotions/micro/service/impl/PromotionsActivityServiceImpl.java
@@ -0,0 +1,14 @@
+package com.shlf.promotions.micro.service.impl;
+
+/**
+ * <p>
+ * 促销活动表 服务实现类
+ * </p>
+ *
+ * @author zhu jie
+ * @since 2018-05-24
+ */
+public class PromotionsActivityServiceImpl {
+
+
+}
